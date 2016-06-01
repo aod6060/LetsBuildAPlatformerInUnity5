a3a832e44fb5ca81ac3f4180887bd0489d2423fa
aod6060 <a.o.d.6060@gmail.com>
2016-05-31 22:47:07 -0500

    Video 1 Content.

---
+++ Platformer Prototype/Assets/script/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 4.0f;
+	// Use this for initialization
+	void Start () {
+	}
+
+    void FixedUpdate()
+    {
+        Rigidbody body = this.GetComponent<Rigidbody>();
+
+        float x = Input.GetAxis("Movement") * Time.fixedDeltaTime * speed;
+
+        transform.Translate(x, 0, 0);
+    }
+}
